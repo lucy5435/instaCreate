93538a9cb6112a1e9e60533850585f09e3ee84f3
Yusuf Usta <46392667+Quiec@users.noreply.github.com>
2019-04-30 21:48:32 +0300

    Update class.php

--- class.php
+++ class.php
@@ -1,8 +1,11 @@
 <?php
 class instaCreator {
-
-	protected $csrf_token = '4j78Wn7q2dd72wyXLred0Llz7tudss1g';
-	protected $mid_token = 'XKnIoAABAAGfcQxrne0VokoQF05f';
+// Buraları Düzenleyin !
+protected $trProxy = "88.255.182.170"; // TR olması zorunludur.
+protected $trProxy_Port = "8080";
+protected $trProxy_Tur = "HTTPS";
+	protected $csrf_token = '8T1YSTEUhZm0D13VLpqncYF0eaHxwfmf'; 
+	protected $mid_token = 'XMby6wABAAHpx_WQyBTy4tSCj7Bl';
 
 	private function connectInstagram($username, $password, $email, $full_name, $proxy){
 
@@ -32,6 +35,7 @@ class instaCreator {
 		$headers[] = "Accept-Language: tr-TR,en-US,en;q=0.8,id;q=0.6";
 
 		curl_setopt($channel, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($channel, CURLOPT_COOKIEJAR, 'cookies/'.$username.'.txt');
 		$result = curl_exec($channel);
 		if(curl_errno($channel)){
 			echo 'Error:' . curl_error($channel);
@@ -39,7 +43,36 @@ class instaCreator {
 		curl_close($channel);
 		return $result;
 	}
+public function bio($username, $id){
+		$channel1 = curl_init();
+		curl_setopt($channel1, CURLOPT_URL, "https://www.instagram.com/web/friendships/$id/follow/");
+		curl_setopt($channel1, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($channel1, CURLOPT_POST, 1);
+		curl_setopt($channel1, CURLOPT_COOKIEFILE, 'cookies/'.$username.'.txt');
+		curl_setopt($channel1, CURLOPT_ENCODING, 'gzip, deflate');
+		$headers = array();
+		$headers[] = "Host: www.instagram.com";
+		$headers[] = "Cookie: fbm_124024574287414=base_domain=.instagram.com; rur=PRN; csrftoken={$this->csrf_token}; mid={$this->mid_token}; fbm_124024574287414=\"base_domain=.instagram.com\"; mcd=1";
+		$headers[] = "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.124 Safari/537.36";
+		$headers[] = "Origin: https://www.instagram.com";
+		$headers[] = "X-Instagram-Ajax: 8958fe1e75ab";
+		$headers[] = "Content-Type: application/x-www-form-urlencoded";
+		$headers[] = "Accept: */*";
+		$headers[] = "X-Requested-With: XMLHttpRequest";
+		$headers[] = "Save-Data: on";
+		$headers[] = "X-Csrftoken: {$this->csrf_token}";
+		$headers[] = "Referer: https://www.instagram.com/accounts/login";
+		$headers[] = "Accept-Language: tr-TR,en-US,en;q=0.8,id;q=0.6";
 
+		curl_setopt($channel1, CURLOPT_HTTPHEADER, $headers); 
+curl_setopt($channel1, CURLOPT_PROXYPORT, $this->trProxy_Port);
+curl_setopt($channel1, CURLOPT_PROXYTYPE, $this->trProxy_Tur); // HTTP/HTTPS türü proxy kullanacağız.
+curl_setopt($channel1, CURLOPT_PROXY, $this->trProxy);
+		$result = curl_exec($channel1);
+		curl_close($channel1);
+		}
+		
+	
 	private function getUser(){
 		$channel = curl_init();
 		curl_setopt($channel, CURLOPT_URL,"http://api.randomuser.me/?nat=tr");
@@ -92,24 +125,27 @@ class instaCreator {
 				$randomUser_Email_Domain = array('yandex.com', 'hotmail.com','gmail.com','icloud.com','outlook.com');
 				$randomUser_Email_Adress = "{$randomUser_User_Name}@".$randomUser_Email_Domain[mt_rand(0, count($randomUser_Email_Domain) - 1)];
 				$randomUser_Password = "Quiec123";
+				$randomUser_Bio = "Yeni+Hesap";
 				$randomUser_Proxy = $this->getProxy();
-				$randomUser_Save_Docs = "users.html";
-
+				$randomUser_Save_Docs = "users.txt";
+$randomUser_id = "5886592160";
+echo "[!] $i Hesap oluşturuluyor!..\n";
 				$userCreate = $this->connectInstagram($randomUser_User_Name, $randomUser_Password, $randomUser_Email_Adress, $randomUser_Full_Name, $randomUser_Proxy);
-				$userCreate = json_decode($userCreate);
+								$userCreate = json_decode($userCreate);
 				if($userCreate->account_created == "true"){
-					$file   = fopen(''.$randomUser_Save_Docs.'', 'r+') or die("file not found!");;
-					$get    = fgets($file);
-					$catat  = fwrite($file, ''.$randomUser_User_Name.':'.$randomUser_Password.'<br>');
-					fclose($file);
-					echo "[!] Bir hesap açıldı. Test ediliyor...";
+fwrite(fopen($randomUser_Save_Docs,"a+") ,"$randomUser_User_Name:$randomUser_Password \n");
+					
 						$kadi = json_decode(file_get_contents('http://insta-node.herokuapp.com/_validate_username?username='.$randomUser_User_Name.''), true)['valid'];
 					if($kadi == false){
-						echo "[!] ".$i.". Açılan hesap: ".$randomUser_User_Name.":".$randomUser_Password."\n";
-						}
+							echo "[!] ".$i.". Açılan hesap: ".$randomUser_User_Name.":".$randomUser_Password."\n";
+							echo "[!] Seçtiğiniz hesapa takip ettiriliyor!\n";
+$this->bio($randomUser_User_Name, $randomUser_id);
+echo "[•] Takip Başarılı\n";
+																	}
 						
 				}else{
 					echo "[!] ".$i.". Açılamıyan hesap: ".$randomUser_User_Name.":".$randomUser_Password."\n";
+					print_r($userCreate);
 				}
 				sleep($sleep);
 			}
