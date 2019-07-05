6edbafe63de90293238f7e5055c0012e11fb0a57
Olcay hIZAL <olcybegendir@gmail.com>
2019-07-05 04:17:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo $homepage;
     $url = 'https://api.ipify.org/?format=text';
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_INTERFACE, "107.23.139.202");
+    //curl_setopt($ch, CURLOPT_INTERFACE, "107.23.139.202");
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
     curl_setopt($ch, CURLOPT_HEADER, 0);
