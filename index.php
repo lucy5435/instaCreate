54c141adbdca7786b8f497d1620069e82a1a6968
Olcay hIZAL <olcybegendir@gmail.com>
2019-07-05 04:24:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,25 @@ echo $homepage;
 
 
 
-    $url = 'https://api.ipify.org/?format=text';
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    //curl_setopt($ch, CURLOPT_INTERFACE, "107.23.139.202");
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_NOBODY, 1);
+    $curl_connection = curl_init("https://api.ipify.org/?format=text");
+    curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
+    curl_setopt($curl_connection, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
+    curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl_connection, CURLOPT_INTERFACE, "107.23.139.202");
+    curl_setopt($curl_connection, CURLOPT_FOLLOWLOCATION, 0);
+    ob_start();
+    $response = curl_exec($curl_connection);
+    ob_end_clean();
+    $err = curl_error($curl_connection);
+    curl_close($curl_connection);
+    if ($err) {return "hata";} else {}
+    if (strpos($response, 'sorry') !== false OR strpos($response, 'Sorry') !== false){
+        return "hata";
+    }else{
+        echo " curl: ". $response;
+    }
 
-    $curl_scraped_page = curl_exec($ch);
-    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-
-    echo " other: ".$curl_scraped_page;
 
 
 
